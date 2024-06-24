d575ad8abceec8894d450a7d788259eda79ef6f6
ameyshrotri <shrotriamey123@gmail.com>
2024-06-24 09:04:13 +0530

    edit feature added

--- index.js
+++ index.js
@@ -7,10 +7,31 @@ app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname)));
+
 app.get("/", function (req, res) {
   fs.readdir(`./files`, function (err, files) {
     res.render("index", { files: files });
   });
+});
+
+app.get("/edit/:filename", function (req, res) {
+  res.render("edit", { filename: req.params.filename });
+});
+
+app.post("/edit", function (req, res) {
+  const previousFileName = `./files/${req.body.previous.trim()}`;
+  const newFileName = `./files/${req.body.new.trim()}`;
+
+  fs.rename(previousFileName, newFileName, function (err) {
+    if (err) {
+      console.error("Error renaming file:", err);
+      res.status(500).send("Error renaming file: " + err.message);
+      return;
+    }
+    res.redirect("/");
+  });
+
+  console.log(req.body);
 });
 
 app.get("/file/:filename", function (req, res) {
@@ -33,4 +54,4 @@ app.post("/create", function (req, res) {
   );
 });
 
-app.listen(3002);
+app.listen(3003);
